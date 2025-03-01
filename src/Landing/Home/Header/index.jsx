f313c3f86e17dda9a5399946a7c4626e8ec9d3d7
Akbarshoh <ismatov2009akb@gmail.com>
2025-03-01 23:34:40 +0500

    Demo Home Header

---
+++ src/Landing/Home/Header/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import styles from "./style.module.scss";
+
+const Home_Header = () => {
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.left}></div>
+
+          <div className={styles.center}>
+            <div className={styles.centerTexts}>
+              <p className={styles.centerText1}>
+                We’re here to help you to be the <span>best one!</span>
+              </p>
+
+              <p className={styles.centerText2}>
+                We harness the latest technologies to develop cutting-edge
+                digital solutions for modern business requirements
+              </p>
+            </div>
+
+            <div></div>
+
+            <div className={styles.numbers}>
+              <div className={styles.centerCard}>
+                <p className={styles.centerCardP1}>1234</p>
+
+                <p className={styles.centerCardP2}>Lorem Ipsum is Simply</p>
+              </div>
+
+              <div className={styles.centerCard}>
+                <p className={styles.centerCardP1}>1234</p>
+
+                <p className={styles.centerCardP2}>Lorem Ipsum is Simply</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.right}></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home_Header;
--- src/Landing/Home/index.jsx
+++ src/Landing/Home/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import styles from "./style.module.scss";
 import LandingNavbar from "../../Layoutes/Navbar";
+import Home_Header from "./Header";
 
 const LandingHome = () => {
   return (
     <>
       <LandingNavbar />
+      <Home_Header />
     </>
   );
 };
